62b892ec915da9b191c406b94275fa94a1dc94c4
Adrian Bailey <59806075+adrian3xl@users.noreply.github.com>
2021-10-07 21:11:08 -0500

    commit
    added some more edits

---
+++ sendemail.php
@@ -0,0 +1,28 @@
+<?php 
+    require 'vendor/autoload.php';
+
+    class SendEmail{
+
+        public static function SendMail($to,$subject,$content){
+            $key = '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';
+
+            $email = new \SendGrid\Mail\Mail();
+            
+            $email->setFrom("adrian.hakeem@gmail.com", "Adrian Bailey");
+            $email->setSubject($subject);
+            $email->addTo($to);
+            $email->addContent("text/plain", $content);
+           
+
+            $sendgrid = new \SendGrid($key);
+
+            try{
+                $response = $sendgrid->send($email);
+                return $response;
+            }catch(Exception $e){
+                echo 'Email exception Caught : '. $e->getMessage() ."\n";
+                return false;
+            }
+        }
+    }
+?>
