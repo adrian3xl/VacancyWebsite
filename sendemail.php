0ef8e0fa1ef8f66c4c8e270ba8fb651cdc0d7d36
Adrian Bailey <59806075+adrian3xl@users.noreply.github.com>
2021-10-09 20:34:13 -0500

    email test
    minor edits

--- sendemail.php
+++ sendemail.php
@@ -6,18 +6,18 @@
         public static function SendMail($to,$subject,$content){
             $key = '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';
 
-            $email = new \SendGrid\Mail\Mail();
+            $ema = new \SendGrid\Mail\Mail();
             
-            $email->setFrom("adrian.hakeem@gmail.com", "Adrian Bailey");
-            $email->setSubject($subject);
-            $email->addTo($to);
-            $email->addContent("text/plain", $content);
+            $ema->setFrom("adrian.hakeem@gmail.com", "Adrian Bailey");
+            $ema->setSubject($subject);
+            $ema->addTo($to);
+            $ema->addContent("text/plain", $content);
            
 
             $sendgrid = new \SendGrid($key);
 
             try{
-                $response = $sendgrid->send($email);
+                $response = $sendgrid->send($ema);
                 return $response;
             }catch(Exception $e){
                 echo 'Email exception Caught : '. $e->getMessage() ."\n";
